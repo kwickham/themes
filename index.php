f30cf11b8868fdd72fbaae885e0309f10c06ced6
kwickham <kwickham@iastate.edu>
2013-03-13 23:36:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en-US">
+<?php get_header(); ?>
 <head>
 <title>Man's First Cousin</title>
 <link rel="stylesheet" type="text/css" href="style.css">
@@ -12,4 +13,5 @@ Man's First Cousin
 </h1>
 </div>
 </body>
+<?php get_footer(); ?>
 </html>
