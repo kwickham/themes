39594660e37d808bfcde1a7669ad2896fbfcabca
Kevin W <kwickham@iastate.edu>
2013-03-19 12:58:55 -0500

    made things more wordpress friendly

---
+++ header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Man's First Cousin</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
--- index.php
+++ index.php
@@ -1,17 +1,73 @@
-<!DOCTYPE html>
-<html lang="en-US">
 <?php get_header(); ?>
-<head>
-<title>Man's First Cousin</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
 <body>
-<div style="header">
-<h1>
-Man's First Cousin
-</h1>
+<div id="container">
+<!--Header-->
+<div id="header" class="clear">
+<h1><a href="#">Man's First Cousin</a></h1>
+<a href="#">Home</a>
+<ul id="nav">
+<li><a href="#">The Writer</a></li>
+<li><a href="#">The Site</a></li>
+<li><a href="#">The Metaphors</a></li>
+</ul>
+</div>
+<!--Search-->
+<div id="search-box" class="clear">
+<h3>Looking for something?</h3>
+<input type="button" value="Quest On!" class"submit"/>
+</div>
+<!--Sidebar-->
+<div id="sidebar" class="clear">
+<ul>
+<li>subscribe</li>
+<li>topics</li>
+<li>archives</li>
+<li>connect</li>
+<li>links</li>
+</ul>
+</div>
+<!--Updates-->
+<div id="updates" class="clear">
+<h1>Site Updates</h1>
+<p>Fusce viverra mauris et lacus convallis convallis. Donec placerat ipsum non ipsum lobortis aliquam. Suspendisse sollicitudin vulputate volutpat. Nulla semper porttitor nisi vel ultrices. Fusce accumsan faucibus leo ut iaculis. Integer blandit, urna in vehicula semper, augue sapien molestie urna, ac porttitor mi sapien vel leo.</p>
+</div>
+<!--Main Content-->
+<div id="main-content" class="clear">
+<div class="header">
+<h1>Post Title</h1>
+<h4>written by: Author's Name</h4>
+<h4>March 17, 2013</h4>
+</div>
+<div class="content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu ligula eros. Vivamus magna metus, dignissim sit amet pulvinar vitae, adipiscing nec augue. Fusce accumsan massa a leo mattis nec fermentum urna interdum. Fusce mauris turpis, tincidunt a dignissim aliquam, imperdiet non libero. In vestibulum egestas nisl at faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla faucibus adipiscing sagittis. Fusce lobortis neque vitae lectus consequat accumsan id ac neque. Ut fringilla adipiscing tellus, sodales lacinia turpis faucibus sit amet. Cras euismod nisi nec ligula rutrum at commodo nisl pretium. In hac habitasse platea dictumst. Phasellus sed ultricies purus.</p>
+</div>
+</div>
+<!--Latest Posts-->
+<div id="latest-sidebar" class="clear">
+<h3>Latest Posts</h3>
+</div>
+<!--Footer-->
+<div id="footer-container">
+<div id="footer" class="clear>
+<div class="login">
+<h6>Login:</h6>
+<form action="#" method="post">
+<fieldset>
+<label for="username">Username</label>
+<input type="text" name="username" class="username/>
+<label for="password">Password</label>
+<input type="password" name="password" class="password"/>
+<input type="button" value="Onward!" class"submit"/>
+<small><a href="#">Forgot password?</a></small>
+</fieldset>
+</form>
+<!--Contact-->
+<div class="write">
+<h6>Contact us:</h6>
+<p><a href="#">Send us an email</a></p>
+</div>
+</div>
 </div>
 </body>
 <?php get_footer(); ?>
-</html>
+
